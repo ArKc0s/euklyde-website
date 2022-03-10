ec8f7b767c2f901ce870ae3ca833b79093676ca9
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2022-03-10 19:12:35 +0100

    Finished portfolio slides (need to find a fix for navfolio on mobile) Added test images

--- mainscript.js
+++ mainscript.js
@@ -1,17 +1,24 @@
-var selectedProject;
+var selectedProject = document.getElementById("project1");
+var selectedSlide = document.getElementById("project1-body");
+
+selectFolioPage("project1");
 
 
 function selectFolioPage(str) {
-
-    if(selectedProject != null) {
-        selectedProject.classList.remove("c-purple");
-        selectedProject.classList.add("c-light-purple");
-    }
+  
+    selectedProject.classList.remove("c-purple");
+    selectedProject.classList.add("c-light-purple");
+    selectedSlide.classList.add("animation-lock");
+    selectedSlide.classList.add("not-visible");
+    
     
     selectedProject = document.getElementById(str);
+    selectedSlide = document.getElementById(str+"-body");
  
     selectedProject.classList.remove("c-light-purple");
     selectedProject.classList.add("c-purple");
+    selectedSlide.classList.remove("not-visible");
+    selectedSlide.classList.remove("animation-lock");
 
 
   }
